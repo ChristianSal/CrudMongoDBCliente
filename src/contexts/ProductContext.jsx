dde311b4797aee25f9d1e5f72fdcac0610c96798
ChristianSal <75845556+ChristianSal@users.noreply.github.com>
2022-03-22 01:52:28 -0500

    version6

--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -15,7 +15,7 @@ const ProductContextProvider = (props) => {
 
     useEffect(()=>{
         productService.readAll().then((data) => setProducts(data));
-    },[]);
+    },[productService, products]);
 
     const createProduct = (product) =>{
         productService.create(product).then((data) => setProducts([...products,data]))
